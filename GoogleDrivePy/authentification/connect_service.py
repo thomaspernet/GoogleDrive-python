e8676dae130ba31cde9fa8e130468baf1aa958c5
Thomas <t.pernetcoudrier@gmail.com>
2019-03-10 08:47:20 +0100

    add py files

---
+++ GoogleDrivePy/authentification/connect_service.py
@@ -0,0 +1,44 @@
+from google.colab import drive
+from google.colab import auth
+class connect_service:
+	def __init__(self, path_json, scope):
+		self.path_json = path_json
+		self.scole = scope
+	def get_auth(self):
+		"""
+		This function mounts a Google Drive in Google Colab. The objective is
+		to access the json credential directly from Google Drive since Google
+		Colab creates new instance each time, there is no other way to locate
+		the credentialself
+
+		The function also gives access to SDK
+		"""
+
+		drive.mount('/content/gdrive')
+		auth.authenticate_user()
+	def get_service(self):
+		"""
+		This function gives access to Google drive and currently Google doc
+		The path to access the token and credential can be locally
+		or in Googe Drive. By default, the token is stored as 'token' and the
+		credential as 'credentials.json'. They need to have this nameself.
+
+		The scope tells the app what action it can do. ie read only, write, etc
+
+		ADD ERROR MESSAGE
+		"""
+
+		path_token = self.path + "token.json"
+		path_credential = self.path + "credentials.json"
+		store = file.Storage(path_token)
+		creds = store.get()
+		if not creds or creds.invalid:
+			flow = client.flow_from_clientsecrets(path_credential, self.scope)
+			creds = tools.run_flow(flow, store)
+		service = build('drive', 'v3', http=creds.authorize(Http()))
+		service_doc = build('docs', 'v1', http=creds.authorize(Http()))
+		service = {
+            "drive" : service,
+            "doc": service_doc
+			}
+		return service
