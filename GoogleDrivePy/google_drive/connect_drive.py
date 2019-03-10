826106798b0951b370d6b46b504a0ce8596552c1
Thomas <t.pernetcoudrier@gmail.com>
2019-03-10 10:28:44 +0100

    Update connect_drive.py

--- GoogleDrivePy/google_drive/connect_drive.py
+++ GoogleDrivePy/google_drive/connect_drive.py
@@ -6,6 +6,8 @@ from oauth2client import file, client, tools
 class connect_drive:
     def __init__(self, service):
         self.service = service
+        self.service_drive = self.service["drive"]
+        self.service_doc = self.service["doc"]
 
     def create_file_root(self, mime_type, name):
         """
@@ -18,7 +20,7 @@ class connect_drive:
         google-drive-mime-types-listing
 
         """
-        service = self.service["drive"]
+        #service = self.service["drive"]
         media_body = MediaFileUpload(name,
                                  mimetype=mime_type,
                                  resumable=True
@@ -29,7 +31,7 @@ class connect_drive:
         'mimeType': mime_type
         }
 
-        upload = service.files().create(
+        upload = self.service_drive.files().create(
             body= body,
             media_body=media_body,
             fields='id').execute()
@@ -38,3 +40,86 @@ class connect_drive:
         print('File ID: {}'.format(file_ID))
 
         return file_ID
+
+    def move_file(file_ID, folder_ID):
+        """
+        The function moves one file to another folder_ID
+        file_ID: ID of the file to move
+        folder_ID: ID of the destination folder
+        """
+	    # Retrieve the existing parents to remove
+	    file = self.service_drive.files().get(fileId=file_ID,
+                                 fields='parents').execute()
+	    previous_parents = ",".join(file.get('parents'))
+	    # Move the file to the new folder
+	    file = service.files().update(fileId=file_ID,
+                                    addParents=folder_ID,
+                                    removeParents=previous_parents,
+                                    fields='id, parents').execute()
+    def find_folder_id(self, folder_name):
+        """
+        The function find the ID of a folder. In order to maximize the search
+        it is best to give unique name to folder
+        """
+        search = "mimeType = 'application/vnd.google-apps.folder'" \
+        " and name contains '" + str(folder_name) + "'"
+        print(search)
+        page_token = None
+        while True:
+            response = self.service_drive.files().list(
+                                          q= search,
+                                          spaces='drive',
+                                          fields='nextPageToken,' \
+                                          'files(id, name)',
+                                          pageToken=page_token).execute()
+        for file in response.get('files', []):
+        # Process change
+            folder_id =  file.get('id')
+            print('Found file: %s (%s)' % (file.get('name'), folder_id))
+            page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+        return folder_id
+
+    def find_file_id(self, file_name):
+        """
+        The function find the ID of a file. In order to maximize the search
+        it is best to give unique name to file.
+        """
+        search  = "name = '" + str(file_name) + "'"
+        print(search)
+        page_token = None
+        while True:
+            response = self.service_drive.files().list(
+                                          q= search,
+                                          spaces='drive',
+                                          fields='nextPageToken,' \
+                                          'files(id, name)',
+                                          pageToken=page_token).execute()
+        for file in response.get('files', []):
+        # Process change
+            file_id =  file.get('id')
+            print('Found file: %s (%s)' % (file.get('name'), file_id))
+            page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+      return file_id
+
+    def move_file(self, file_name, folder_name):
+        """
+        This function move one file from root to another folder .
+        The function uses find_folder_id and find_file_id to get the IDs
+        """
+        ### get folder ID
+        folder_id = self.find_folder_id(folder_name)
+        ###
+        file_id = self.find_file_id(file_name)
+	    # Retrieve the existing parents to remove
+	    file = self.service_drive.files().get(fileId = file_id,
+                                 fields = 'parents').execute()
+	    previous_parents = ",".join(file.get('parents'))
+	    # Move the file to the new folder
+	    file = self.service_drive.files().update(fileId = file_id,
+                                    addParents = folder_id,
+                                    removeParents = previous_parents,
+                                    fields = 'id, parents').execute()
