a3e10981419ff1099331c4504a3e24c6013ca065
Thomas <t.pernetcoudrier@gmail.com>
2019-03-10 09:40:54 +0100

    Update connect_drive.py

--- GoogleDrivePy/google_drive/connect_drive.py
+++ GoogleDrivePy/google_drive/connect_drive.py
@@ -1,5 +1,7 @@
 from googleapiclient.discovery import build
 from oauth2client import file, client, tools
+from google.auth.transport.requests import Request
+from httplib2 import Http
 from apiclient.http import MediaFileUpload
 class connect_drive:
     def __init__(self, service):
