c5f3ac4f921da914cf73acec221daa7a63a27c26
Thomas <t.pernetcoudrier@gmail.com>
2019-03-11 07:13:00 +0100

    Update connect_service_colab.py

--- GoogleDrivePy/google_authentification/connect_service_colab.py
+++ GoogleDrivePy/google_authentification/connect_service_colab.py
@@ -2,9 +2,9 @@ from google.colab import drive
 from google.colab import auth
 
 class connect_service_colab:
-	def __init__(self, path_json, scope):
-		self.path_json = path_json
-		self.scole = scope
+	def __init__(self):
+		#self.path_json = path_json
+		#self.scope = scope
 	def get_auth(self):
 		"""
 		This function mounts a Google Drive in Google Colab. The objective is
