6f449d40d9700c92eb529eb85d10e6d26f2da92f
Thomas <t.pernetcoudrier@gmail.com>
2019-03-16 15:52:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ required_package = ['pandas', 'oauth2client', 'httplib2',
 
 setuptools.setup(
      name='GoogleDrivePy',
-     version='0.9',
+     version='1.0',
      #scripts=['data_analysis_econometrics'] ,
      author="Thomas Pernet",
      author_email="t.pernetcoudrier@gmail.com",
