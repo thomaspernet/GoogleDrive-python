2b3918b4e6ae011927d1ab33a26fe3563bcda589
Thomas <t.pernetcoudrier@gmail.com>
2019-08-11 10:01:03 +0200

    Update connect_drive.py

--- GoogleDrivePy/google_drive/connect_drive.py
+++ GoogleDrivePy/google_drive/connect_drive.py
@@ -6,10 +6,10 @@ import re
 import pandas as pd
 
 alphabet = [
-'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB',
-'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'
-]
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB',
+		'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'
+	]
 
 class connect_drive:
 	def __init__(self, service, verbose =  True):
@@ -19,7 +19,7 @@ class connect_drive:
 		self.service_sheet = self.service["sheet"]
 		self.verbose = verbose
 
-		def upload_file_root(self, mime_type, file_name):
+	def upload_file_root(self, mime_type, file_name):
 		"""
 		The function creates a file in the root of Google Drive.
 		mime_type: You can use MIME types to filter query results or
@@ -32,9 +32,9 @@ class connect_drive:
 		"""
 		#service = self.service["drive"]
 		media_body = MediaFileUpload(file_name,
-			mimetype=mime_type,
-			resumable=True
-			)
+								 mimetype=mime_type,
+								 resumable=True
+								 )
 		body = {
 		'title': file_name,
 		'name': file_name,
@@ -51,7 +51,7 @@ class connect_drive:
 
 		return file_ID
 
-		def find_folder_id(self, folder_name, to_print = True):
+	def find_folder_id(self, folder_name, to_print = True):
 		"""
 		The function find the ID of a folder. In order to maximize the search
 		it is best to give unique name to folder
@@ -61,24 +61,24 @@ class connect_drive:
 		page_token = None
 		while True:
 			response = self.service_drive.files().list(
-				q= search,
-				spaces='drive',
-				fields='nextPageToken,' \
-				'files(id, name)',
-				pageToken=page_token).execute()
+										  q= search,
+										  spaces='drive',
+										  fields='nextPageToken,' \
+										  'files(id, name)',
+										  pageToken=page_token).execute()
 			for file in response.get('files', []):
 		# Process change
-		folder_id =  file.get('id')
-		if to_print:
-			print('Found file: %s (%s)' % (file.get('name'), folder_id))
-			page_token = response.get('nextPageToken', None)
-			return folder_id
+				folder_id =  file.get('id')
+				if to_print:
+					print('Found file: %s (%s)' % (file.get('name'), folder_id))
+				page_token = response.get('nextPageToken', None)
+				return folder_id
 			if page_token is None:
 				break
-				print('Folder {} not found'.format(folder_name))
+		print('Folder {} not found'.format(folder_name))
 
 
-				def find_file_id(self, file_name, to_print = True):
+	def find_file_id(self, file_name, to_print = True):
 		"""
 		The function find the ID of a file. In order to maximize the search
 		it is best to give unique name to file.
@@ -87,23 +87,23 @@ class connect_drive:
 		page_token = None
 		while True:
 			response = self.service_drive.files().list(
-				q= search,
-				spaces='drive',
-				fields='nextPageToken,' \
-				'files(id, name)',
-				pageToken=page_token).execute()
+										  q= search,
+										  spaces='drive',
+										  fields='nextPageToken,' \
+										  'files(id, name)',
+										  pageToken=page_token).execute()
 			for file in response.get('files', []):
 		# Process change
-		file_id =  file.get('id')
-		if to_print:
-			print('Found file: %s (%s)' % (file.get('name'), file_id))
-			page_token = response.get('nextPageToken', None)
-			return file_id
+				file_id =  file.get('id')
+				if to_print:
+					print('Found file: %s (%s)' % (file.get('name'), file_id))
+				page_token = response.get('nextPageToken', None)
+				return file_id
 			if page_token is None:
 				break
-				print('File {} not found'.format(file_name))
+		print('File {} not found'.format(file_name))
 
-				def move_file(self, file_name, folder_name):
+	def move_file(self, file_name, folder_name):
 		"""
 		This function move one file from root to another folder .
 		The function uses find_folder_id and find_file_id to get the IDs
@@ -112,22 +112,22 @@ class connect_drive:
 		try:
 			folder_id = self.find_folder_id(folder_name)
 		###
-		file_id = self.find_file_id(file_name)
+			file_id = self.find_file_id(file_name)
 		# Retrieve the existing parents to remove
-		file = self.service_drive.files().get(fileId = file_id,
-			fields = 'parents').execute()
-		previous_parents = ",".join(file.get(
-			'parents'))
+			file = self.service_drive.files().get(fileId = file_id,
+								 fields = 'parents').execute()
+			previous_parents = ",".join(file.get(
+								 'parents'))
 		# Move the file to the new folder
-		file = self.service_drive.files().update(fileId = file_id,
-			addParents = folder_id,
-			removeParents = previous_parents,
-			fields = 'id, parents').execute()
-		print('File {} moved to {}'.format(file_name, folder_name))
-	except:
-		print('Impossible to move {} in {}'.format(file_name, folder_name))
+			file = self.service_drive.files().update(fileId = file_id,
+									addParents = folder_id,
+									removeParents = previous_parents,
+									fields = 'id, parents').execute()
+			print('File {} moved to {}'.format(file_name, folder_name))
+		except:
+			print('Impossible to move {} in {}'.format(file_name, folder_name))
 
-		def access_google_doc(self, doc_name):
+	def access_google_doc(self, doc_name):
 		"""
 		The function searches for an existing document. If the
 		document is not found, then one is created
@@ -136,15 +136,15 @@ class connect_drive:
 		if doc_id is None:
 			title = doc_name
 			body = {
-			'title': title
-			}
+				  'title': title
+				  }
 			doc = self.service_doc.documents() \
-			.create(body=body).execute()
+			  .create(body=body).execute()
 			doc_id = self.find_file_id(file_name = doc_name, to_print = False)
 			print('Created document {} with name {}'.format(doc_id, title))
-			return doc_id
+		return doc_id
 
-			def add_image_to_doc(self, image_name, doc_name):
+	def add_image_to_doc(self, image_name, doc_name):
 		"""
 		The function finds or creates a new Google docs and appends images.
 		The image should be saved in Google drive
@@ -163,14 +163,14 @@ class connect_drive:
 		index = content["content"][1]['paragraph']['elements'][0]['endIndex']
 		# Retrieve the documents contents from the Docs service.
 		requests = [
-		{
-		'insertInlineImage': {
-		'location': {
-		'index': index - 1
-		},
-		'uri':
-		url
-		}}
+			{
+			'insertInlineImage': {
+			'location': {
+				'index': index - 1
+			},
+			'uri':
+				url
+			}}
 		]
 
 		# Execute the request.
@@ -178,7 +178,7 @@ class connect_drive:
 			documentId=doc_id, body={'requests': requests}).execute()
 		print('Image added to {}'.format(doc_name))
 
-		def add_bullet_to_doc(self, doc_name, name_bullet= "Hello world"):
+	def add_bullet_to_doc(self, doc_name, name_bullet= "Hello world"):
 		"""
 		Shows basic usage of the Docs API.
 		Prints the title of a sample document.
@@ -189,30 +189,30 @@ class connect_drive:
 		content  = document.get('body')
 		index = content["content"][1]['paragraph']['elements'][0]['endIndex']
 		requests = [
-		{
-		'insertText': {
-		'location': {
-		'index': 1
-		},
-		'text': str(name_bullet) + '\n',
-		}}, {
-		'createParagraphBullets': {
-		'range': {
-		'startIndex': 1,
-		'endIndex':  index + 1
-		},
-		'bulletPreset': 'BULLET_ARROW_DIAMOND_DISC',
+			 {
+				'insertText': {
+					'location': {
+					'index': 1
+					},
+					'text': str(name_bullet) + '\n',
+					}}, {
+				'createParagraphBullets': {
+					'range': {
+						'startIndex': 1,
+						'endIndex':  index + 1
+						},
+				'bulletPreset': 'BULLET_ARROW_DIAMOND_DISC',
+			}
 		}
-		}
-		]
+	]
 
 	# Execute the request.
-	result = self.service_doc.documents().batchUpdate(
-		documentId=doc_id, body={'requests': requests}).execute()
-	print('Bullet point added to {}'.format(doc_name))
+		result = self.service_doc.documents().batchUpdate(
+			documentId=doc_id, body={'requests': requests}).execute()
+		print('Bullet point added to {}'.format(doc_name))
 
 	def add_data_to_spreadsheet(self, data, sheetID, sheetName,
-		detectRange = True, rangeData = None):
+                            detectRange = True, rangeData = None):
     """
     Add Data to an existing Spreadsheet
     - Data: Pandas Dataframe
@@ -222,47 +222,47 @@ class connect_drive:
     - detectRange: Boolean: If True, then automatically detect range
     - rangeData: By default None, else User define range 'SHEETNAME$AA00:BB11'
         - Not, it should include one row for the header!
-        """
-        
+    """
+
     ### Check if sheetName exit
     sheet_metadata = self.service_sheet.spreadsheets().get(
-    	spreadsheetId = sheetID
-    	).execute()
+			  spreadsheetId = sheetID
+			  ).execute()
 
     sheets = sheet_metadata.get('sheets', '')
     list_sheets = [sheets[x].get("properties", {}).get("title", {})
-    for x in range(0, len(sheets))]
+				 for x in range(0, len(sheets))]
 
     if not sheetName in list_sheets:
 
-    	data_s = {'requests': [
-    	{
-    	'addSheet':{
-    	'properties':{'title': sheetName}
-    	}
-    	}
-    	]}
+        data_s = {'requests': [
+            {
+			 'addSheet':{
+				'properties':{'title': sheetName}
+                }
+            }
+		]}
 	## Add new sheet
-	self.service_sheet.spreadsheets().batchUpdate(
-		spreadsheetId= sheetID,
-		body=data_s
-		).execute()
-	
+        self.service_sheet.spreadsheets().batchUpdate(
+			  spreadsheetId= sheetID,
+			  body=data_s
+			).execute()
+
     ### Make Data to list: Google engine needs JSON serialized
-    
+
     data_list = data.to_numpy().tolist()
-    headers_name = list(data)        
-    
-    ### Get size of the data 
+    headers_name = list(data)
+
+    ### Get size of the data
     total_rows, total_col  = data.shape
-    
+
     #### If detect range
     if detectRange:
-    	
+
         ### Get size of the sheet
         nb_cols, n_row = self.getRowAndColumns(sheetID = sheetID,
-        	sheetName = sheetName)
-        
+                                          sheetName = sheetName)
+
         ### range does not exist
         if n_row == 1:
             ### The sheet is empty so we use the number of columns
@@ -272,45 +272,45 @@ class connect_drive:
             ### add header
             data_list.insert(0, headers_name)
         else:
-        	nb_cols = nb_cols
-        	first_cell = n_row + 1
+            nb_cols = nb_cols
+            first_cell = n_row + 1
 
-        	for i, letter in enumerate(alphabet):
-        		if i == nb_cols:
-        			range_ = letter
-        			if i + 2 == nb_cols:
-        				range_ = letter
-        				
-        				last_rows = n_row + total_rows
+        for i, letter in enumerate(alphabet):
+            if i == nb_cols:
+                range_ = letter
+            if i + 2 == nb_cols:
+                range_ = letter
 
-        				range_sprs = '{0}!A{1}:{2}{3}'
+        last_rows = n_row + total_rows
+
+        range_sprs = '{0}!A{1}:{2}{3}'
         ### Add one for the first row to write
         range_sprs = range_sprs.format(sheetName, first_cell, range_, last_rows)
     else:
-    	range_sprs = rangeData
+        range_sprs = rangeData
         ### if user add custom range, then need to add headers
         data_list.insert(0, headers_name)
-        
+
     ### Add to Spreadsheet
     data_ = [
-    {
-    'range': range_sprs,
-    'values': data_list
-    },
+	  {
+		'range': range_sprs,
+		'values': data_list
+      },
 	# Additional ranges to update ...
-	]
-	body = {
-	'valueInputOption': 'RAW',
-	'data': data_
-	}
+  ]
+    body = {
+        'valueInputOption': 'RAW',
+        'data': data_
+    }
 
-	result = self.service_sheet.spreadsheets().values().batchUpdate(
-		spreadsheetId= sheetID, body=body).execute()
-	
-	print('Range {0} has been updated.'.format(range_sprs))
+    result = self.service_sheet.spreadsheets().values().batchUpdate(
+			spreadsheetId= sheetID, body=body).execute()
+
+    print('Range {0} has been updated.'.format(range_sprs))
 
 	def upload_data_from_spreadsheet(self, sheetID, sheetName,
-		to_dataframe = False):
+	 to_dataframe = False):
 		"""
 		Upload data from Google spreadsheet
 		If to_dataframe then return the data to a dataframe.
@@ -324,26 +324,26 @@ class connect_drive:
 		for i, letter in enumerate(alphabet):
 			if i == nb_cols:
 				range_ = letter
-				if i + 2 == nb_cols:
-					range_ = letter
+			if i + 2 == nb_cols:
+				range_ = letter
 
-					range_sprs = '{0}!A1:{1}{2}'
-					range_sprs = range_sprs.format(sheetName, range_, n_row)
+		range_sprs = '{0}!A1:{1}{2}'
+		range_sprs = range_sprs.format(sheetName, range_, n_row)
 
-					load_spreadsheets = self.service_sheet.spreadsheets().values().get(
-						spreadsheetId=sheetID,
-						range=range_sprs).execute()
+		load_spreadsheets = self.service_sheet.spreadsheets().values().get(
+			spreadsheetId=sheetID,
+			range=range_sprs).execute()
 
-					if to_dataframe:
+		if to_dataframe:
 
-						load_spreadsheets = pd.DataFrame(
-							load_spreadsheets.get('values', []),columns =
-							load_spreadsheets['values'][0]).drop([0])
+			load_spreadsheets = pd.DataFrame(
+			load_spreadsheets.get('values', []),columns =
+						load_spreadsheets['values'][0]).drop([0])
 
-						return load_spreadsheets
+		return load_spreadsheets
 
 
-						def getLatestRow(self, sheetID, sheetName):
+	def getLatestRow(self, sheetID, sheetName):
 		"""
 		The option includeGridData = True in the get elements return a dictionary
 		with the row sort_values
@@ -351,17 +351,17 @@ class connect_drive:
 		"""
 
 		gridData = self.service_sheet.spreadsheets().get(
-			spreadsheetId = sheetID, includeGridData = True).execute()
+		spreadsheetId = sheetID, includeGridData = True).execute()
 
 		sheets = gridData.get('sheets', '')
 		list_sheets = [sheets[x].get("properties", {}).get("title", {})
-		for x in range(0, len(sheets))]
+			   for x in range(0, len(sheets))]
 
 		index_sheet = list_sheets.index(sheetName)
 
 		try:
 			#latestRow = gridData['sheets'][
-			#index_sheet]['properties']['gridProperties']['rowCount'] 
+			#index_sheet]['properties']['gridProperties']['rowCount']
 			### Property above includes all rows, empty included
 			latestRow = len(gridData['sheets'][index_sheet]['data'][0]['rowData'])
 			#len(
@@ -369,10 +369,10 @@ class connect_drive:
 		except:
 			latestRow = 1
 
-			return latestRow
+		return latestRow
 
 
-			def getColumnNumber(self, sheetID, sheetName):
+	def getColumnNumber(self, sheetID, sheetName):
 		"""
 		The option includeGridData = True in the get elements return a dictionary
 		with the row sort_values
@@ -380,11 +380,11 @@ class connect_drive:
 		"""
 
 		gridData = self.service_sheet.spreadsheets().get(
-			spreadsheetId = sheetID, includeGridData = True).execute()
+		spreadsheetId = sheetID, includeGridData = True).execute()
 
 		sheets = gridData.get('sheets', '')
 		list_sheets = [sheets[x].get("properties", {}).get("title", {})
-		for x in range(0, len(sheets))]
+			   for x in range(0, len(sheets))]
 
 		index_sheet = list_sheets.index(sheetName)
 
@@ -394,13 +394,13 @@ class connect_drive:
 
 			### Same a rows, count only non empty cols
 			columnNumber = len(gridData['sheets'][
-				index_sheet]['data'][0]['rowData'][0]['values'])
+			index_sheet]['data'][0]['rowData'][0]['values'])
 		except:
 			columnNumber = 1
 
-			return columnNumber
+		return columnNumber
 
-			def getRowAndColumns(self, sheetID, sheetName):
+	def getRowAndColumns(self, sheetID, sheetName):
 		"""
 		The option includeGridData = True in the get elements return a dictionary
 		with the row sort_values
@@ -408,21 +408,21 @@ class connect_drive:
 		"""
 
 		gridData = self.service_sheet.spreadsheets().get(
-			spreadsheetId = sheetID, includeGridData = True).execute()
+		spreadsheetId = sheetID, includeGridData = True).execute()
 
 		sheets = gridData.get('sheets', '')
 		list_sheets = [sheets[x].get("properties", {}).get("title", {})
-		for x in range(0, len(sheets))]
+			   for x in range(0, len(sheets))]
 
 		index_sheet = list_sheets.index(sheetName)
 
 		try:
 			columnNumber = len(gridData['sheets'][
-				index_sheet]['data'][0]['rowData'][0]['values'])
+			index_sheet]['data'][0]['rowData'][0]['values'])
 
 			latestRow = len(gridData['sheets'][index_sheet]['data'][0]['rowData'])
 		except:
 			columnNumber = 1
 			latestRow = 1
 
-			return columnNumber, latestRow
+		return columnNumber, latestRow
