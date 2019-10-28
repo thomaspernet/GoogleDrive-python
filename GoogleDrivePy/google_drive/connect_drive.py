4ef8af02f714edd4f1a1bd2cb6ab95e19af37773
thomaspernet <t.pernetcoudrier@gmail.com>
2019-10-28 20:04:15 +0100

    Update connect_drive.py

--- GoogleDrivePy/google_drive/connect_drive.py
+++ GoogleDrivePy/google_drive/connect_drive.py
@@ -8,7 +8,8 @@ import pandas as pd
 alphabet = [
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB',
-		'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'
+		'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ','AK', 'AL', 'AM','AN',
+		'AO','AP','AQ','AR', 'AS','AS','AU'
 	]
 
 class connect_drive:
