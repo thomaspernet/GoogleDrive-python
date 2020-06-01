626cf4ae524c37abec547f36be834e3d38240796
thomaspernet <t.pernetcoudrier@gmail.com>
2020-06-01 06:58:15 +0200

    change name functions

--- GoogleDrivePy/google_authorization/connect_service.py
+++ GoogleDrivePy/google_authorization/connect_service.py
@@ -7,23 +7,26 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 #from httplib2 import Http
 from google.cloud import storage, bigquery
-class connect_service_local:
+
+class connect_service:
 	def __init__(self, path_credential = None,path_service_account = None,
 	 scope = None):
 		"""
-		path_credential: connect to Google Drive and associated project: GSpreadhseet/GDoc
-		service_account: connect to google cloud Service: BQ/GCS
+		path_credential: connect to Google Drive and associated project:
+		GSpreadhseet/GDoc. This is the pickle file containing the token
+		path_service_account: connect to google cloud Service: BQ/GCS. This is
+		the json file containing the credentials
 		"""
 		self.path_credential = path_credential
 		self.path_service_account = path_service_account
 		self.scope = scope
 
-	def get_storage_client(self):
+	def connection_gcp(self, verbose = False):
 		"""
-		This function gives access to Google platform by passing the credential
+		This function gives access to Google Cloud platform by passing the credential
 		to from_service_account_json
 		The path_service_account needs to be the full name. It can be downloaded
-		from the API authentification
+		from the API authorization
 		"""
 		storage_client = storage.Client.from_service_account_json(
 		self.path_service_account)
@@ -33,15 +36,20 @@ class connect_service_local:
 			"Storage_account" : storage_client,
 			"bigquery_account" : bigquery_client,
 			}
-		print('Service account storage and Bigquery are now connected. \n' \
-		'Service account storage is stored as {} and accessible with "Storage_account" \n' \
-		'Service account Bigquery is stored as {} and accessible with "bigquery_account"'.format(
+		if verbose:
+			print("""
+			Service account storage and Bigquery are now connected.\n
+		'Service account storage is stored as {} and accessible with
+		"Storage_account"
+		'Service account Bigquery is stored as {} and accessible with
+		"bigquery_account"""
+		.format(
 		service_account["Storage_account"],
 		service_account["bigquery_account"])
 		)
 		return service_account
 
-	def get_service(self):
+	def connection_drive(self, verbose = False):
 		"""
 		This function gives access to Google drive and currently Google doc
 		The path to access the token and credential can be locally
@@ -58,20 +66,11 @@ class connect_service_local:
 
 		ADD ERROR MESSAGE
 		"""
-		#path_token = self.path_json + "token.json"
-		#store = file.Storage(path_token)
-		#creds = store.get()
-		#if not creds or creds.invalid:
-		#	path_credential = self.path_json + "credentials.json"
-		#	flow = client.flow_from_clientsecrets(path_credential, self.scope)
-		#	creds = tools.run_flow(flow, store)
 		creds = None
-		# The file token.pickle stores the user's access and refresh tokens, and is
-		# created automatically when the authorization flow completes for the first
-		# time.
 
-		if os.path.exists(self.path_credential+'token.pickle'):
-			with open(self.path_credential+'token.pickle', 'rb') as token:
+		path_pickle = os.path.join(self.path_credential, 'token.pickle')
+		if os.path.exists(path_pickle):
+			with open(path_pickle, 'rb') as token:
 				creds = pickle.load(token)
 		# If there are no (valid) credentials available, let the user log in.
 		if not creds or not creds.valid:
@@ -94,12 +93,15 @@ class connect_service_local:
 			"doc": service_doc,
 			"sheet": service_excel
 			}
-		print('Service Google Drive and Docs, Sheet are now connected. \n' \
-		'Service Google Drive is stored as {} and accessible with "drive" \n' \
-		'Service Google Doc is stored as {} and accessible with "doc" \n' \
-		'Service Google Sheet is stored as {}and accessible with "sheet"'.format(
-		service["drive"],
-		service["doc"],
-		service["sheet"])
-		)
+		if verbose:
+			print("""
+			Service Google Drive and Docs, Sheet are now connected.\n
+			'Service Google Drive is stored as {} and accessible with "drive"\n
+			'Service Google Doc is stored as {} and accessible with "doc"\n
+			'Service Google Sheet is stored as {}and accessible with "sheet"""
+			.format(
+			service["drive"],
+			service["doc"],
+			service["sheet"])
+			)
 		return service
